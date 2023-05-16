b1c81cac08ca5681fe10e8e052ebcbb438d5ba41
Alexandre Santos <alexandrehsantos@gmail.com>
2023-05-16 17:41:00 -0300

    added output

---
+++ src/main/java/bulvee/com/tests/sumarray/SumArray.java
@@ -0,0 +1,22 @@
+package bulvee.com.tests.sumarray;
+
+import java.util.stream.Stream;
+
+public class SumArray {
+    public static void main(String[] args) {
+        int[] addedArray = runningSum(new int[]{1,2,3,4});
+        for(int i =0; i < addedArray.length; i++){
+            System.out.println(addedArray[i]);
+        }
+    }
+
+    public static int[] runningSum(int[] nums) {
+        int sum =0;
+        int[] addedArray = new int[nums.length];
+        for(int i =0; i < nums.length; i++){
+            sum += i;
+            addedArray[i] = sum;
+        }
+        return addedArray;
+    }
+}
